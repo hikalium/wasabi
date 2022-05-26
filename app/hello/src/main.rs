66d44f8cf843f60d027aecd95447ea4f50d91838
hikalium <hikalium@hikalium.com>
2022-05-27 00:08:39 +0900

    update

--- app/hello/src/main.rs
+++ app/hello/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+// Needed to solve: can't find crate for `std`
+#![no_std]
+#![feature(start)]
+
+// Needed for implementing panic()
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    unimplemented!();
+}
+
+#[start]
+fn main(_: isize, _: *const *const u8) -> isize {
+    loop {}
 }
