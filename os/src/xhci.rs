b73236d39a9c6b7e638ddbfb2d83e529db1cfb14
hikalium <hikalium@hikalium.com>
2022-08-10 20:06:44 +0900

    update

--- os/src/xhci.rs
+++ os/src/xhci.rs
@@ -1,5 +1,6 @@
 extern crate alloc;
 
+use crate::arch::x86_64::busy_loop_hint;
 use crate::arch::x86_64::paging::with_current_page_table;
 use crate::arch::x86_64::paging::PageAttr;
 use crate::error::Result;
@@ -11,7 +12,9 @@ use crate::pci::VendorDeviceId;
 use crate::println;
 use crate::util::extract_bits;
 use alloc::boxed::Box;
+use core::mem::ManuallyDrop;
 use core::ptr::read_volatile;
+use core::ptr::write_volatile;
 
 #[allow(dead_code)]
 #[derive(Debug, Copy, Clone)]
@@ -27,6 +30,63 @@ struct CapabilityRegisters {
     cap_params2: u32,
 }
 const _: () = assert!(core::mem::size_of::<CapabilityRegisters>() == 0x20);
+
+#[allow(dead_code)]
+#[derive(Debug, Copy, Clone)]
+#[repr(C)]
+struct OperationalRegisters {
+    command: u32,
+    status: u32,
+    page_size: u32,
+    rsvdz1: [u32; 2],
+    notification_ctrl: u32,
+    cmd_ring_ctrl: u64,
+    rsvdz2: [u64; 2],
+    device_ctx_base_addr_array_ptr: u64,
+    config: u64,
+}
+impl OperationalRegisters {
+    fn clear_command_bits(&mut self, bits: u32) {
+        unsafe {
+            write_volatile(&mut self.command, self.command() & !bits);
+        }
+    }
+    fn set_command_bits(&mut self, bits: u32) {
+        unsafe {
+            write_volatile(&mut self.command, self.command() | bits);
+        }
+    }
+    fn command(&mut self) -> u32 {
+        unsafe { read_volatile(&self.command) }
+    }
+    fn status(&mut self) -> u32 {
+        unsafe { read_volatile(&self.status) }
+    }
+}
+const _: () = assert!(core::mem::size_of::<OperationalRegisters>() == 0x40);
+
+#[allow(dead_code)]
+#[derive(Debug, Copy, Clone)]
+#[repr(C)]
+struct InterrupterRegisterSet {
+    management: u32,
+    moderation: u32,
+    erst_size: u32,
+    rsvdp: u32,
+    erst_base: u64,
+    erdp: u64,
+}
+const _: () = assert!(core::mem::size_of::<InterrupterRegisterSet>() == 0x20);
+#[allow(dead_code)]
+#[derive(Debug, Copy, Clone)]
+#[repr(C)]
+struct RuntimeRegisters {
+    microframe_index: u32,
+    rsvdz: [u32; 7],
+    irs: [InterrupterRegisterSet; 1024],
+}
+const _: () = assert!(core::mem::size_of::<RuntimeRegisters>() == 0x8020);
+//  static_assert(offsetof(RuntimeRegisters, irs) == 0x20);
 
 pub struct XhciDriver {}
 impl XhciDriver {
@@ -66,6 +126,9 @@ pub struct XhciDriverInstance {
     bdf: BusDeviceFunction,
     max_slots: usize,
     max_ports: usize,
+    max_num_scratch_pad_bufs: usize,
+    cap_regs: ManuallyDrop<Box<CapabilityRegisters>>,
+    op_regs: ManuallyDrop<Box<OperationalRegisters>>,
 }
 impl XhciDriverInstance {
     // https://wiki.osdev.org/RTL8139
@@ -85,23 +148,48 @@ impl XhciDriverInstance {
         }
         println!("page_table updated!");
 
-        let cap_regs = unsafe { &*(bar0.addr() as *const CapabilityRegisters) };
-        println!("cap_regs @ {:p}", cap_regs);
-        println!("params[0] @ {:p}", &cap_regs.params[0]);
-
+        let cap_regs =
+            unsafe { ManuallyDrop::new(Box::from_raw(bar0.addr() as *mut CapabilityRegisters)) };
+        let op_regs = unsafe {
+            ManuallyDrop::new(Box::from_raw(
+                bar0.addr().add(cap_regs.length as usize) as *mut OperationalRegisters
+            ))
+        };
         let hcs_params1 = unsafe { read_volatile(&cap_regs.params[0] as *const u32) };
-        println!("hcs_params1 = {:X}", hcs_params1);
         let max_slots = extract_bits(hcs_params1, 24, 8) as usize;
         let max_ports = extract_bits(hcs_params1, 0, 8) as usize;
 
-        let di = XhciDriverInstance {
+        let hcs_params2 = unsafe { read_volatile(&cap_regs.params[0] as *const u32) };
+        let max_num_scratch_pad_bufs =
+            (extract_bits(hcs_params2, 21, 5) << 5 | extract_bits(hcs_params2, 27, 5)) as usize;
+
+        let mut xhc = XhciDriverInstance {
             bdf,
             max_slots,
             max_ports,
+            max_num_scratch_pad_bufs,
+            cap_regs,
+            op_regs,
         };
-        println!("{:?}", di);
+        println!("{:?}", xhc);
+        xhc.reset();
 
         unimplemented!()
+    }
+    fn reset(&mut self) {
+        const CMD_RUN_STOP: u32 = 0b0001;
+        const CMD_HC_RESET: u32 = 0b0010;
+        const STATUS_HC_HALTED: u32 = 0b0001;
+        println!("[xHC] Resetting the controller...");
+        self.op_regs.clear_command_bits(CMD_RUN_STOP);
+        while self.op_regs.status() & STATUS_HC_HALTED == 0 {
+            busy_loop_hint();
+        }
+        self.op_regs.set_command_bits(CMD_HC_RESET);
+        while self.op_regs.command() & CMD_HC_RESET != 0 {
+            busy_loop_hint();
+        }
+        println!("[xHC] Reset done!");
     }
 }
 impl PciDeviceDriverInstance for XhciDriverInstance {
