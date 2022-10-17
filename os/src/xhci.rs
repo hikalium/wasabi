e727403853918aafd1dd44c5e587e4bc8c8f8063
hikalium <hikalium@hikalium.com>
2022-10-18 00:44:36 +0900

    update

--- os/src/xhci.rs
+++ os/src/xhci.rs
@@ -6,7 +6,6 @@ pub mod ring;
 pub mod trb;
 
 use crate::allocator::ALLOCATOR;
-use crate::arch::x86_64::busy_loop_hint;
 use crate::arch::x86_64::paging::IoBox;
 use crate::arch::x86_64::paging::Mmio;
 use crate::error::Result;
@@ -15,7 +14,6 @@ use crate::executor::yield_execution;
 use crate::executor::Executor;
 use crate::executor::Task;
 use crate::hpet::Hpet;
-use crate::pci::BarMem64;
 use crate::pci::BusDeviceFunction;
 use crate::pci::Pci;
 use crate::pci::PciDeviceDriver;
@@ -30,10 +28,8 @@ use crate::usb::EndpointDescriptor;
 use crate::usb::InterfaceDescriptor;
 use crate::usb::IntoPinnedMutableSlice;
 use crate::usb::UsbDescriptor;
-use crate::util::extract_bits;
 use alloc::alloc::Layout;
 use alloc::boxed::Box;
-use alloc::fmt;
 use alloc::fmt::Debug;
 use alloc::format;
 use alloc::rc::Rc;
@@ -46,9 +42,7 @@ use core::convert::AsRef;
 use core::future::Future;
 use core::marker::PhantomPinned;
 use core::mem::size_of;
-use core::mem::transmute;
 use core::pin::Pin;
-use core::ptr::read_volatile;
 use core::ptr::write_volatile;
 use core::slice;
 use core::task::Context;
@@ -61,9 +55,11 @@ use context::RawDeviceContextBaseAddressArray;
 use context::SlotContext;
 use registers::CapabilityRegisters;
 use registers::OperationalRegisters;
+use registers::PortLinkState;
+use registers::PortSc;
+use registers::PortScIteratorItem;
+use registers::PortState;
 use registers::RuntimeRegisters;
-use regs::PortLinkState;
-use regs::PortState;
 use ring::CommandRing;
 use ring::EventRing;
 use ring::TransferRing;
@@ -202,204 +198,6 @@ impl PciDeviceDriver for XhciDriver {
     }
 }
 
-mod regs {
-    use super::busy_loop_hint;
-    use super::extract_bits;
-    use super::fmt;
-    use super::format;
-    use super::println;
-    use super::read_volatile;
-    use super::transmute;
-    use super::write_volatile;
-    use super::BarMem64;
-    use super::CapabilityRegisters;
-    use super::Debug;
-    use super::Result;
-    use super::UsbMode;
-    use super::WasabiError;
-
-    #[repr(u32)]
-    #[non_exhaustive]
-    #[derive(Debug)]
-    #[allow(unused)]
-    pub enum PortLinkState {
-        U0 = 0,
-        U1,
-        U2,
-        U3,
-        Disabled,
-        RxDetect,
-        Inactive,
-        Polling,
-        Recovery,
-        HotReset,
-        ComplianceMode,
-        TestMode,
-        Resume = 15,
-    }
-    #[repr(C)]
-    pub struct PortScWrapper {
-        ptr: *mut u32,
-    }
-    #[derive(Debug)]
-    pub enum PortState {
-        // Figure 4-25: USB2 Root Hub Port State Machine
-        PoweredOff,
-        Disconnected,
-        Reset,
-        Disabled,
-        Enabled,
-        Other {
-            pp: bool,
-            ccs: bool,
-            ped: bool,
-            pr: bool,
-        },
-    }
-    impl PortScWrapper {
-        const BIT_CURRENT_CONNECT_STATUS: u32 = 1 << 0;
-        const BIT_PORT_ENABLED_DISABLED: u32 = 1 << 1;
-        const BIT_PORT_RESET: u32 = 1 << 4;
-        const BIT_PORT_POWER: u32 = 1 << 9;
-        pub fn value(&self) -> u32 {
-            unsafe { read_volatile(self.ptr) }
-        }
-        pub fn set_bits(&self, bits: u32) {
-            let old = self.value();
-            unsafe { write_volatile(self.ptr, old | bits) }
-        }
-        pub fn reset(&self) {
-            self.set_bits(Self::BIT_PORT_POWER);
-            while !self.pp() {
-                busy_loop_hint();
-            }
-            self.set_bits(Self::BIT_PORT_RESET);
-            while self.pr() {
-                busy_loop_hint();
-            }
-        }
-        pub fn ccs(&self) -> bool {
-            // CCS - Current Connect Status - ROS
-            self.value() & Self::BIT_CURRENT_CONNECT_STATUS != 0
-        }
-        pub fn ped(&self) -> bool {
-            // PED - Port Enabled/Disabled - RW1CS
-            self.value() & Self::BIT_PORT_ENABLED_DISABLED != 0
-        }
-        pub fn pr(&self) -> bool {
-            // PR - Port Reset - RW1S
-            self.value() & Self::BIT_PORT_RESET != 0
-        }
-        pub fn pls(&self) -> PortLinkState {
-            // PLS - Port Link Status - RWS
-            unsafe { transmute(extract_bits(self.value(), 5, 4)) }
-        }
-        pub fn pp(&self) -> bool {
-            // PP - Port Power - RWS
-            self.value() & Self::BIT_PORT_POWER != 0
-        }
-        pub fn port_speed(&self) -> UsbMode {
-            // Port Speed - ROS
-            // Returns Protocol Speed ID (PSI). See 7.2.1 of xhci spec.
-            // Default mapping is in Table 7-13: Default USB Speed ID Mapping.
-            match extract_bits(self.value(), 10, 4) {
-                1 => UsbMode::FullSpeed,
-                2 => UsbMode::LowSpeed,
-                3 => UsbMode::HighSpeed,
-                4 => UsbMode::SuperSpeed,
-                v => UsbMode::Unknown(v),
-            }
-        }
-        pub fn max_packet_size(&self) -> Result<u16> {
-            match self.port_speed() {
-                UsbMode::FullSpeed | UsbMode::LowSpeed => Ok(8),
-                UsbMode::HighSpeed => Ok(64),
-                UsbMode::SuperSpeed => Ok(512),
-                speed => Err(WasabiError::FailedString(format!(
-                    "Unknown Protocol Speeed ID: {:?}",
-                    speed
-                ))),
-            }
-        }
-        pub fn state(&self) -> PortState {
-            // 4.19.1.1 USB2 Root Hub Port
-            match (self.pp(), self.ccs(), self.ped(), self.pr()) {
-                (false, false, false, false) => PortState::PoweredOff,
-                (true, false, false, false) => PortState::Disconnected,
-                (true, true, false, false) => PortState::Disabled,
-                (true, true, false, true) => PortState::Reset,
-                (true, true, true, false) => PortState::Enabled,
-                tuple => {
-                    let (pp, ccs, ped, pr) = tuple;
-                    PortState::Other { pp, ccs, ped, pr }
-                }
-            }
-        }
-    }
-    impl Debug for PortScWrapper {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            write!(
-                f,
-                "PORTSC: value={:#010X}, state={:?}, link_state={:?}, mode={:?}",
-                self.value(),
-                self.state(),
-                self.pls(),
-                self.port_speed(),
-            )
-        }
-    }
-    // Iterator over PortSc
-    pub struct PortScIteratorItem {
-        pub port: usize,
-        pub portsc: PortScWrapper,
-    }
-    pub struct PortScIterator<'a> {
-        list: &'a PortSc,
-        next_index: usize,
-    }
-    impl<'a> Iterator for PortScIterator<'a> {
-        type Item = PortScIteratorItem;
-        fn next(&mut self) -> Option<Self::Item> {
-            let port = self.next_index + 1;
-            let portsc = self.list.get(port).ok()?;
-            self.next_index += 1;
-            Some(PortScIteratorItem { port, portsc })
-        }
-    }
-    // Interface to access PORTSC registers
-    pub struct PortSc {
-        base: *mut u32,
-        num_ports: usize,
-    }
-    impl PortSc {
-        pub fn new(bar: &BarMem64, cap_regs: &CapabilityRegisters) -> Self {
-            let base = unsafe { bar.addr().add(cap_regs.length()).add(0x400) } as *mut u32;
-            let num_ports = cap_regs.num_of_ports();
-            println!("PORTSC @ {:p}, max_port_num = {}", base, num_ports);
-            Self { base, num_ports }
-        }
-        pub fn get(&self, port: usize) -> Result<PortScWrapper> {
-            // [xhci] 5.4.8: PORTSC
-            // OperationalBase + (0x400 + 0x10 * (n - 1))
-            // where n = Port Number (1, 2, ..., MaxPorts)
-            if (1..=self.num_ports).contains(&port) {
-                Ok(PortScWrapper {
-                    ptr: unsafe { self.base.add((port - 1) * 4) },
-                })
-            } else {
-                Err("xHC: Port Number Out of Range".into())
-            }
-        }
-        pub fn iter(&self) -> PortScIterator {
-            PortScIterator {
-                list: self,
-                next_index: 0,
-            }
-        }
-    }
-}
-
-// EndpointType: 0-7
 #[derive(Debug, Copy, Clone)]
 #[repr(u8)]
 #[derive(PartialEq, Eq)]
@@ -427,32 +225,12 @@ impl From<&EndpointDescriptor> for EndpointType {
     }
 }
 
-#[derive(PartialEq, Eq, Debug, Copy, Clone)]
-pub enum UsbMode {
-    Unknown(u32),
-    FullSpeed,
-    LowSpeed,
-    HighSpeed,
-    SuperSpeed,
-}
-impl UsbMode {
-    fn psi(&self) -> u32 {
-        match *self {
-            Self::FullSpeed => 1,
-            Self::LowSpeed => 2,
-            Self::HighSpeed => 3,
-            Self::SuperSpeed => 4,
-            Self::Unknown(psi) => psi,
-        }
-    }
-}
-
 pub struct Xhci {
     _bdf: BusDeviceFunction,
     cap_regs: Mmio<CapabilityRegisters>,
     op_regs: Mmio<OperationalRegisters>,
     rt_regs: Mmio<RuntimeRegisters>,
-    portsc: regs::PortSc,
+    portsc: PortSc,
     doorbell_regs: Mmio<[u32; 256]>,
     command_ring: CommandRing,
     primary_event_ring: EventRing,
@@ -480,7 +258,7 @@ impl Xhci {
         let doorbell_regs = unsafe {
             Mmio::from_raw(bar0.addr().add(cap_regs.as_ref().dboff()) as *mut [u32; 256])
         };
-        let portsc = regs::PortSc::new(&bar0, cap_regs.as_ref());
+        let portsc = PortSc::new(&bar0, cap_regs.as_ref());
         let scratchpad_buffers = Self::alloc_scratch_pad_buffers(
             op_regs.as_ref().page_size()?,
             cap_regs.as_ref().num_scratch_pad_bufs(),
@@ -919,15 +697,15 @@ impl Xhci {
         Ok(())
     }
     async fn poll(&mut self) -> Result<()> {
-        let mut port_to_enable: Option<regs::PortScIteratorItem> = None;
-        for regs::PortScIteratorItem { port, portsc } in self.portsc.iter() {
+        let mut port_to_enable: Option<PortScIteratorItem> = None;
+        for PortScIteratorItem { port, portsc } in self.portsc.iter() {
             let state = portsc.state();
-            if let regs::PortState::Disabled = state {
+            if let PortState::Disabled = state {
                 // Reset port to Enable the port (via Reset state)
-                port_to_enable = Some(regs::PortScIteratorItem { port, portsc });
+                port_to_enable = Some(PortScIteratorItem { port, portsc });
             }
         }
-        if let Some(regs::PortScIteratorItem { port, portsc }) = port_to_enable {
+        if let Some(PortScIteratorItem { port, portsc }) = port_to_enable {
             println!(
                 "Resetting port: prev state: portsc[port = {}] = {:#10X} {:?} {:?}",
                 port,
--- os/src/xhci/registers.rs
+++ os/src/xhci/registers.rs
@@ -1,6 +1,9 @@
+extern crate alloc;
+
 use crate::arch::x86_64::busy_loop_hint;
 use crate::error::Result;
 use crate::error::WasabiError;
+use crate::pci::BarMem64;
 use crate::print;
 use crate::println;
 use crate::util::extract_bits;
@@ -9,9 +12,213 @@ use crate::xhci::ring::CommandRing;
 use crate::xhci::ring::EventRing;
 use crate::xhci::DeviceContextBaseAddressArray;
 use crate::xhci::RawDeviceContextBaseAddressArray;
+use alloc::fmt;
+use alloc::fmt::Debug;
+use alloc::format;
 use core::mem::size_of;
+use core::mem::transmute;
 use core::ptr::read_volatile;
 use core::ptr::write_volatile;
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+pub enum UsbMode {
+    Unknown(u32),
+    FullSpeed,
+    LowSpeed,
+    HighSpeed,
+    SuperSpeed,
+}
+impl UsbMode {
+    pub fn psi(&self) -> u32 {
+        match *self {
+            Self::FullSpeed => 1,
+            Self::LowSpeed => 2,
+            Self::HighSpeed => 3,
+            Self::SuperSpeed => 4,
+            Self::Unknown(psi) => psi,
+        }
+    }
+}
+
+#[repr(u32)]
+#[non_exhaustive]
+#[derive(Debug)]
+#[allow(unused)]
+pub enum PortLinkState {
+    U0 = 0,
+    U1,
+    U2,
+    U3,
+    Disabled,
+    RxDetect,
+    Inactive,
+    Polling,
+    Recovery,
+    HotReset,
+    ComplianceMode,
+    TestMode,
+    Resume = 15,
+}
+#[repr(C)]
+pub struct PortScWrapper {
+    ptr: *mut u32,
+}
+#[derive(Debug)]
+pub enum PortState {
+    // Figure 4-25: USB2 Root Hub Port State Machine
+    PoweredOff,
+    Disconnected,
+    Reset,
+    Disabled,
+    Enabled,
+    Other {
+        pp: bool,
+        ccs: bool,
+        ped: bool,
+        pr: bool,
+    },
+}
+impl PortScWrapper {
+    const BIT_CURRENT_CONNECT_STATUS: u32 = 1 << 0;
+    const BIT_PORT_ENABLED_DISABLED: u32 = 1 << 1;
+    const BIT_PORT_RESET: u32 = 1 << 4;
+    const BIT_PORT_POWER: u32 = 1 << 9;
+    pub fn value(&self) -> u32 {
+        unsafe { read_volatile(self.ptr) }
+    }
+    pub fn set_bits(&self, bits: u32) {
+        let old = self.value();
+        unsafe { write_volatile(self.ptr, old | bits) }
+    }
+    pub fn reset(&self) {
+        self.set_bits(Self::BIT_PORT_POWER);
+        while !self.pp() {
+            busy_loop_hint();
+        }
+        self.set_bits(Self::BIT_PORT_RESET);
+        while self.pr() {
+            busy_loop_hint();
+        }
+    }
+    pub fn ccs(&self) -> bool {
+        // CCS - Current Connect Status - ROS
+        self.value() & Self::BIT_CURRENT_CONNECT_STATUS != 0
+    }
+    pub fn ped(&self) -> bool {
+        // PED - Port Enabled/Disabled - RW1CS
+        self.value() & Self::BIT_PORT_ENABLED_DISABLED != 0
+    }
+    pub fn pr(&self) -> bool {
+        // PR - Port Reset - RW1S
+        self.value() & Self::BIT_PORT_RESET != 0
+    }
+    pub fn pls(&self) -> PortLinkState {
+        // PLS - Port Link Status - RWS
+        unsafe { transmute(extract_bits(self.value(), 5, 4)) }
+    }
+    pub fn pp(&self) -> bool {
+        // PP - Port Power - RWS
+        self.value() & Self::BIT_PORT_POWER != 0
+    }
+    pub fn port_speed(&self) -> UsbMode {
+        // Port Speed - ROS
+        // Returns Protocol Speed ID (PSI). See 7.2.1 of xhci spec.
+        // Default mapping is in Table 7-13: Default USB Speed ID Mapping.
+        match extract_bits(self.value(), 10, 4) {
+            1 => UsbMode::FullSpeed,
+            2 => UsbMode::LowSpeed,
+            3 => UsbMode::HighSpeed,
+            4 => UsbMode::SuperSpeed,
+            v => UsbMode::Unknown(v),
+        }
+    }
+    pub fn max_packet_size(&self) -> Result<u16> {
+        match self.port_speed() {
+            UsbMode::FullSpeed | UsbMode::LowSpeed => Ok(8),
+            UsbMode::HighSpeed => Ok(64),
+            UsbMode::SuperSpeed => Ok(512),
+            speed => Err(WasabiError::FailedString(format!(
+                "Unknown Protocol Speeed ID: {:?}",
+                speed
+            ))),
+        }
+    }
+    pub fn state(&self) -> PortState {
+        // 4.19.1.1 USB2 Root Hub Port
+        match (self.pp(), self.ccs(), self.ped(), self.pr()) {
+            (false, false, false, false) => PortState::PoweredOff,
+            (true, false, false, false) => PortState::Disconnected,
+            (true, true, false, false) => PortState::Disabled,
+            (true, true, false, true) => PortState::Reset,
+            (true, true, true, false) => PortState::Enabled,
+            tuple => {
+                let (pp, ccs, ped, pr) = tuple;
+                PortState::Other { pp, ccs, ped, pr }
+            }
+        }
+    }
+}
+impl Debug for PortScWrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "PORTSC: value={:#010X}, state={:?}, link_state={:?}, mode={:?}",
+            self.value(),
+            self.state(),
+            self.pls(),
+            self.port_speed(),
+        )
+    }
+}
+// Iterator over PortSc
+pub struct PortScIteratorItem {
+    pub port: usize,
+    pub portsc: PortScWrapper,
+}
+pub struct PortScIterator<'a> {
+    list: &'a PortSc,
+    next_index: usize,
+}
+impl<'a> Iterator for PortScIterator<'a> {
+    type Item = PortScIteratorItem;
+    fn next(&mut self) -> Option<Self::Item> {
+        let port = self.next_index + 1;
+        let portsc = self.list.get(port).ok()?;
+        self.next_index += 1;
+        Some(PortScIteratorItem { port, portsc })
+    }
+}
+// Interface to access PORTSC registers
+pub struct PortSc {
+    base: *mut u32,
+    num_ports: usize,
+}
+impl PortSc {
+    pub fn new(bar: &BarMem64, cap_regs: &CapabilityRegisters) -> Self {
+        let base = unsafe { bar.addr().add(cap_regs.length()).add(0x400) } as *mut u32;
+        let num_ports = cap_regs.num_of_ports();
+        println!("PORTSC @ {:p}, max_port_num = {}", base, num_ports);
+        Self { base, num_ports }
+    }
+    pub fn get(&self, port: usize) -> Result<PortScWrapper> {
+        // [xhci] 5.4.8: PORTSC
+        // OperationalBase + (0x400 + 0x10 * (n - 1))
+        // where n = Port Number (1, 2, ..., MaxPorts)
+        if (1..=self.num_ports).contains(&port) {
+            Ok(PortScWrapper {
+                ptr: unsafe { self.base.add((port - 1) * 4) },
+            })
+        } else {
+            Err("xHC: Port Number Out of Range".into())
+        }
+    }
+    pub fn iter(&self) -> PortScIterator {
+        PortScIterator {
+            list: self,
+            next_index: 0,
+        }
+    }
+}
 
 #[derive(Copy, Clone)]
 #[repr(C)]
