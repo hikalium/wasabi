04facb69979552d47c656a43c7afb36b94f2fc10
hikalium <hikalium@hikalium.com>
2022-10-21 00:52:59 +0900

    save

--- os/src/mutex.rs
+++ os/src/mutex.rs
@@ -75,8 +75,8 @@ impl<T: Sized> Mutex<T> {
     pub fn lock(&self) -> MutexGuard<T> {
         self.try_lock().expect("unimplemented!")
     }
-    pub fn under_locked<R>(&self, f: &dyn FnOnce(&mut T) -> Result<R>) -> Result<R> {
-        let locked = self.lock();
+    pub fn under_locked<R: Sized>(&self, f: &dyn Fn(&mut T) -> Result<R>) -> Result<R> {
+        let mut locked = self.lock();
         f(&mut *locked)
     }
 }
