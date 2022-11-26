7a9b869cb81659e7d3807716b8f7416a8ad818d0
hikalium <hikalium@hikalium.com>
2022-11-26 22:03:55 +0900

    update

--- os/src/rtl8139.rs
+++ os/src/rtl8139.rs
@@ -10,6 +10,7 @@ use crate::error::Result;
 use crate::executor::Task;
 use crate::executor::TimeoutFuture;
 use crate::executor::ROOT_EXECUTOR;
+use crate::mutex::Mutex;
 use crate::network::ArpPacket;
 use crate::network::EthernetAddr;
 use crate::network::IpV4Addr;
@@ -22,6 +23,7 @@ use crate::println;
 use crate::util::Sliceable;
 use alloc::boxed::Box;
 use alloc::collections::VecDeque;
+use alloc::rc::Rc;
 use core::mem::size_of;
 use core::mem::MaybeUninit;
 use core::pin::Pin;
@@ -52,19 +54,45 @@ impl PciDeviceDriver for Rtl8139Driver {
 // RTL8139_RXBUF_SIZE == 8192+16+1500 for WRAP=1
 // See https://wiki.osdev.org/RTL8139 for more info
 const RTL8139_RXBUF_SIZE: usize = 9708;
+struct RxContext {
+    buf: Pin<Box<[u8; RTL8139_RXBUF_SIZE]>>,
+    next_index: usize,
+    packet_count: usize,
+}
+impl RxContext {
+    fn new() -> Self {
+        Self {
+            buf: Box::pin(unsafe { MaybeUninit::zeroed().assume_init() }),
+            next_index: 0,
+            packet_count: 0,
+        }
+    }
+}
+
+struct TxContext {
+    pending_packets: VecDeque<Box<[u8]>>,
+    queued_packets: [Option<Pin<Box<[u8]>>>; 4],
+    next_index: usize,
+    packet_count: usize,
+}
+impl TxContext {
+    fn new() -> Self {
+        const PACKET_NONE: Option<Pin<Box<[u8]>>> = None;
+        Self {
+            pending_packets: VecDeque::new(),
+            queued_packets: [PACKET_NONE; 4],
+            next_index: 0,
+            packet_count: 0,
+        }
+    }
+}
+
 struct Rtl8139 {
     _bdf: BusDeviceFunction,
     io_base: u16,
     eth_addr: EthernetAddr,
-    //
-    rx_buf: Pin<Box<[u8; RTL8139_RXBUF_SIZE]>>,
-    next_rx_byte_idx: usize,
-    rx_packet_count: usize,
-    //
-    tx_pending_packets: VecDeque<Box<[u8]>>,
-    tx_queued_packets: [Option<Pin<Box<[u8]>>>; 4],
-    next_tx_reg_idx: usize,
-    tx_packet_count: usize,
+    rx: Mutex<RxContext>,
+    tx: Mutex<TxContext>,
 }
 impl Rtl8139 {
     // https://wiki.osdev.org/RTL8139
@@ -94,22 +122,17 @@ impl Rtl8139 {
         }
         println!("Software Reset Done!");
 
-        const PACKET_NONE: Option<Pin<Box<[u8]>>> = None;
         let d = Self {
             _bdf: bdf,
             io_base,
             eth_addr,
-            //
-            rx_buf: Box::pin(unsafe { MaybeUninit::zeroed().assume_init() }),
-            next_rx_byte_idx: 0,
-            rx_packet_count: 0,
-            //
-            tx_pending_packets: VecDeque::new(),
-            tx_queued_packets: [PACKET_NONE; 4],
-            next_tx_reg_idx: 0,
-            tx_packet_count: 0,
+            rx: Mutex::new(RxContext::new()),
+            tx: Mutex::new(TxContext::new()),
         };
-        let rx_buf_ptr = d.rx_buf.as_ref().as_ptr();
+        let rx_buf_ptr = {
+            let rx = d.rx.lock();
+            rx.buf.as_ref().as_ptr()
+        };
         assert!((rx_buf_ptr as usize) < ((u32::MAX) as usize - RTL8139_RXBUF_SIZE));
         println!("rx_buffer is at {:#p}", rx_buf_ptr);
         write_io_port_u32(io_base + 0x30, rx_buf_ptr as usize as u32);
@@ -119,15 +142,21 @@ impl Rtl8139 {
 
         Ok(d)
     }
-    fn queue_packet(&mut self, packet: Box<[u8]>) -> Result<()> {
-        self.tx_pending_packets.push_back(packet);
+    fn queue_packet(&self, packet: Box<[u8]>) -> Result<()> {
+        let mut tx = self.tx.lock();
+        tx.pending_packets.push_back(packet);
         Ok(())
     }
-    async fn poll(&mut self) -> Result<()> {
-        // Tx operations
+    fn update_rx_buf_read_ptr(&self, rp: *const u8) {
+        // CAPR: Current Address of Packet Read
+        write_io_port_u32(self.io_base + 0x38, u32::try_from(rp as usize).unwrap());
+    }
+    fn poll_tx(&self) -> Result<()> {
+        let mut tx = self.tx.lock();
         loop {
             // Fill Tx Queue as much as possible
-            let tx_cmd = read_io_port_u32(self.io_base + (0x10 + 4 * self.next_tx_reg_idx) as u16);
+            let index = tx.next_index;
+            let tx_cmd = read_io_port_u32(self.io_base + (0x10 + 4 * index) as u16);
             // bit 0-12
             // - packet size. 1792 is the max size
             // bit 13 (OWND):
@@ -145,46 +174,53 @@ impl Rtl8139 {
             // Transmission completed > (Some(_), 1, 1)
             // Queued packet discarded > (None, _, _)
 
-            let qp = &mut self.tx_queued_packets[self.next_tx_reg_idx];
-            if qp.is_some() && tx_cmd & (1 << 13) != 0 && tx_cmd & (1 << 15) != 0 {
-                // Tx operation is done, release the entry
-                *qp = None;
-                println!("Tx[{}] done!", self.next_tx_reg_idx);
-            }
-            if qp.is_some() {
-                // No more Tx entry are available.
-                break;
+            if tx.queued_packets[index].is_some() {
+                println!("tx_cmd[{}] == {:#06X}", index, tx_cmd);
+                if tx_cmd & (1 << 13) != 0 && tx_cmd & (1 << 15) != 0 {
+                    // Tx operation is done, release the entry
+                    tx.queued_packets[index] = None;
+                    println!("Tx[{}] done!", index);
+                } else {
+                    // No more Tx entry are available.
+                    println!("No more tx entry. tx_cmd[{}] == {:#06X}", index, tx_cmd);
+                    break;
+                }
             }
             // qp is None == we can send a packet
-            if let Some(next_packet) = self.tx_pending_packets.pop_front() {
+            if let Some(next_packet) = tx.pending_packets.pop_front() {
                 // Enqueue Tx packet
                 let next_packet = Pin::new(next_packet);
                 let packet_len: u32 = next_packet.as_ref().len().try_into()?;
                 write_io_port_u32(
-                    self.io_base + 0x20 + 4 * self.next_tx_reg_idx as u16, /* TSAD[self.next_tx_reg_idx], buf addr */
+                    self.io_base + 0x20 + 4 * index as u16, /* TSAD[self.next_tx_reg_idx], buf addr */
                     (next_packet.as_ref().get_ref().as_ptr() as usize).try_into()?,
                 );
+                core::sync::atomic::fence(core::sync::atomic::Ordering::SeqCst);
                 write_io_port_u32(
-                    self.io_base + 0x10 + 4 * self.next_tx_reg_idx as u16, /* TSD[self.next_tx_reg_idx], size + flags */
+                    self.io_base + 0x10 + 4 * index as u16, /* TSD[self.next_tx_reg_idx], size + flags */
                     packet_len,
                 );
                 println!(
                     "Tx[{}] queued! packet_size = {}, #{}",
-                    self.next_tx_reg_idx, packet_len, self.tx_packet_count
+                    index, packet_len, tx.packet_count
                 );
-                *qp = Some(next_packet);
-                self.next_tx_reg_idx = (self.next_tx_reg_idx + 1) % 4;
-                self.tx_packet_count += 1;
+                println!("tx_cmd[{}] == {:#06X}", index, tx_cmd);
+                tx.queued_packets[index] = Some(next_packet);
+                tx.next_index = (index + 1) % 4;
+                tx.packet_count += 1;
             } else {
                 // No more packets to send
+                println!("No more packets to send");
                 break;
             }
         }
-
-        // Rx Operations
+        Ok(())
+    }
+    fn poll_rx(&self) -> Result<()> {
+        let mut rx = self.rx.lock();
         loop {
-            let rx_buf_ptr = self.rx_buf.as_ref().as_ptr();
-            let rx_desc_ptr = unsafe { rx_buf_ptr.add(self.next_rx_byte_idx) };
+            let rx_buf_ptr = rx.buf.as_ref().as_ptr();
+            let rx_desc_ptr = unsafe { rx_buf_ptr.add(rx.next_index) };
             let rx_status = unsafe { *(rx_desc_ptr.add(0) as *const u16) };
             if rx_status == 0 {
                 break;
@@ -201,18 +237,24 @@ impl Rtl8139 {
             let packet_len = unsafe { *(rx_desc_ptr.offset(2) as *const u16) } as usize;
             println!(
                 "rx_status: {}, packet_len: {}, next_rx_byte_idx: {}, #{}",
-                rx_status, packet_len, self.next_rx_byte_idx, self.rx_packet_count
+                rx_status, packet_len, rx.next_index, rx.packet_count,
             );
             let arp = unsafe { slice::from_raw_parts(rx_desc_ptr.offset(4), packet_len) };
             if packet_len >= size_of::<ArpPacket>() {
                 let arp = ArpPacket::from_bytes(&arp[0..size_of::<ArpPacket>()]);
                 println!("{arp:?}");
             }
-            self.next_rx_byte_idx += packet_len + 4;
-            self.rx_packet_count += 1;
+            self.update_rx_buf_read_ptr(rx_desc_ptr);
+            rx.next_index += packet_len + 4;
+            rx.packet_count += 1;
         }
-
-        TimeoutFuture::new_ms(100).await;
+        Ok(())
+    }
+    async fn poll(&self) -> Result<()> {
+        println!("poll!");
+        self.poll_tx()?;
+        self.poll_rx()?;
+        TimeoutFuture::new_ms(1000).await;
         Ok(())
     }
 }
@@ -220,14 +262,14 @@ impl Rtl8139 {
 pub struct Rtl8139DriverInstance {}
 impl Rtl8139DriverInstance {
     fn new(bdf: BusDeviceFunction) -> Result<Self> {
-        let mut d = Rtl8139::new(bdf)?;
+        let d = Rc::new(Rtl8139::new(bdf)?);
         (*ROOT_EXECUTOR.lock()).spawn(Task::new(async move {
-            let arp_req = Box::pin(ArpPacket::request(
-                d.eth_addr,
-                IpV4Addr::new([10, 0, 2, 15]),
-                IpV4Addr::new([10, 0, 2, 2]),
-            ));
-            for _ in 0..200 {
+            for _ in 0..100 {
+                let arp_req = Box::pin(ArpPacket::request(
+                    d.eth_addr,
+                    IpV4Addr::new([10, 0, 2, 15]),
+                    IpV4Addr::new([10, 0, 2, 2]),
+                ));
                 d.queue_packet(arp_req.copy_into_slice())?;
             }
 
