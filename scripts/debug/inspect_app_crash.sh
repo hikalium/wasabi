fb5bd1642bd4253d8506b06975975e7fdd4e3f21
hikalium <hikalium@hikalium.com>
2022-12-18 11:35:51 +0900

    update

--- scripts/debug/inspect_app_crash.sh
+++ scripts/debug/inspect_app_crash.sh
@@ -1,16 +1,11 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 RET_TO_IN_MEMORY=`cat qemu_debug.log | grep hikalium | grep ret_to | cut -d ':' -f 3`
-echo ${RET_TO_IN_MEMORY}
 ENTRY_POINT_IN_MEMORY=`cat com2.log | grep -a -e entry_point | cut -d '=' -f 2`
-echo ${ENTRY_POINT_IN_MEMORY}
 ENTRY_POINT_IN_OBJDUMP=0x`make objdump_hello | grep '<entry>:' | cut -d ' ' -f 1`
-echo ${ENTRY_POINT_IN_OBJDUMP}
 RET_TO_IN_OBJDUMP=`printf "0x%X\n" $((${RET_TO_IN_MEMORY} - ${ENTRY_POINT_IN_MEMORY} + ${ENTRY_POINT_IN_OBJDUMP}))`
-echo ${RET_TO_IN_OBJDUMP}
-
 LINE_RET_TO="$(make objdump_hello | grep "`echo ${RET_TO_IN_OBJDUMP} | sed -E 's/^0x//' | tr '[:upper:]' '[:lower:]'`")"
-echo "${LINE_RET_TO}"
-
 ADDR_RET_TO="`echo ${LINE_RET_TO} | cut -d ' ' -f 1`"
+FN_SYMBOL=`make objdump_hello | grep -E -e ">:$" -e "${ADDR_RET_TO}" | grep -B 1 -e "${ADDR_RET_TO}" | head -n 1`
 
-make objdump_hello | grep -E -e ">:$" -e "${ADDR_RET_TO}" | grep -B 1 -e "${ADDR_RET_TO}"
+echo ${FN_SYMBOL}
+make objdump_hello | grep -B 1 -e "${ADDR_RET_TO}"
