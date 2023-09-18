e88240ee3c5f65b7c43b1af86a1d1427148f7aab
hikalium <hikalium@hikalium.com>
2023-09-18 21:01:08 +0900

    Update SerialPort

--- os/src/init.rs
+++ os/src/init.rs
@@ -10,7 +10,8 @@ use crate::hpet;
 use crate::memory_map_holder;
 use crate::pci::Pci;
 use crate::println;
-use crate::serial;
+use crate::serial::SerialPort;
+use crate::serial::SerialPortIndex;
 use crate::text_area;
 use crate::text_area::TextArea;
 use crate::util;
@@ -28,6 +29,7 @@ use crate::x86_64::paging::PML4;
 use crate::x86_64::CpuidRequest;
 use alloc::boxed::Box;
 use core::cmp::max;
+use core::fmt::Write;
 use core::pin::Pin;
 use core::slice;
 use efi::EfiMemoryType::CONVENTIONAL_MEMORY;
@@ -130,7 +132,16 @@ pub fn init_with_boot_services(
     image_handle: efi::EfiHandle,
     efi_system_table: Pin<&'static efi::EfiSystemTable>,
 ) {
-    serial::com_initialize(serial::IO_ADDR_COM2);
+    {
+        let mut serial = SerialPort::new(SerialPortIndex::Com1);
+        serial.init();
+        writeln!(serial, "WasabiOS COM1 Initialized").expect("Failed to print out to COM1");
+    }
+    {
+        let mut serial = SerialPort::new(SerialPortIndex::Com2);
+        serial.init();
+        writeln!(serial, "WasabiOS COM2 Initialized").expect("Failed to print out to COM2");
+    }
     let kernel_stack = efi::alloc_pages(
         efi_system_table,
         size_in_pages_from_bytes(KERNEL_STACK_SIZE),
--- os/src/serial.rs
+++ os/src/serial.rs
@@ -5,54 +5,44 @@ use core::convert::TryInto;
 use core::fmt;
 
 // https://wiki.osdev.org/Serial_Ports
-pub const IO_ADDR_COM1: u16 = 0x3f8;
-pub const IO_ADDR_COM2: u16 = 0x2f8;
-pub const IO_ADDR_COM3: u16 = 0x3e8;
-pub const IO_ADDR_COM4: u16 = 0x2e8;
-pub const IO_ADDR_COM5: u16 = 0x5f8;
-pub const IO_ADDR_COM6: u16 = 0x4f8;
-pub const IO_ADDR_COM7: u16 = 0x5e8;
-pub const IO_ADDR_COM8: u16 = 0x4e8;
-pub const IO_ADDR_COM: [u16; 8] = [
-    IO_ADDR_COM1,
-    IO_ADDR_COM2,
-    IO_ADDR_COM3,
-    IO_ADDR_COM4,
-    IO_ADDR_COM5,
-    IO_ADDR_COM6,
-    IO_ADDR_COM7,
-    IO_ADDR_COM8,
-];
-
-pub fn com_initialize(base_io_addr: u16) {
-    write_io_port_u8(base_io_addr + 1, 0x00); // Disable all interrupts
-    write_io_port_u8(base_io_addr + 3, 0x80); // Enable DLAB (set baud rate divisor)
-    const BAUD_DIVISOR: u16 = 0x0001; // baud rate = (115200 / BAUD_DIVISOR)
-    write_io_port_u8(base_io_addr, (BAUD_DIVISOR & 0xff).try_into().unwrap());
-    write_io_port_u8(base_io_addr + 1, (BAUD_DIVISOR >> 8).try_into().unwrap());
-    write_io_port_u8(base_io_addr + 3, 0x03); // 8 bits, no parity, one stop bit
-    write_io_port_u8(base_io_addr + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
-    write_io_port_u8(base_io_addr + 4, 0x0B); // IRQs enabled, RTS/DSR set
+#[repr(u16)]
+#[derive(Clone, Copy)]
+pub enum SerialPortIndex {
+    Com1 = 0x3f8,
+    Com2 = 0x2f8,
 }
 
 pub struct SerialPort {
-    base_io_addr: u16,
+    index: SerialPortIndex,
 }
 impl SerialPort {
-    pub fn new(base_io_addr: u16) -> Self {
-        Self { base_io_addr }
+    pub fn new(index: SerialPortIndex) -> Self {
+        Self { index }
+    }
+    fn base(&self) -> u16 {
+        self.index as u16
+    }
+    pub fn init(&mut self) {
+        write_io_port_u8(self.base() + 1, 0x00); // Disable all interrupts
+        write_io_port_u8(self.base() + 3, 0x80); // Enable DLAB (set baud rate divisor)
+        const BAUD_DIVISOR: u16 = 0x0001; // baud rate = (115200 / BAUD_DIVISOR)
+        write_io_port_u8(self.base(), (BAUD_DIVISOR & 0xff).try_into().unwrap());
+        write_io_port_u8(self.base() + 1, (BAUD_DIVISOR >> 8).try_into().unwrap());
+        write_io_port_u8(self.base() + 3, 0x03); // 8 bits, no parity, one stop bit
+        write_io_port_u8(self.base() + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
+        write_io_port_u8(self.base() + 4, 0x0B); // IRQs enabled, RTS/DSR set
     }
     pub fn send_char(&self, c: char) {
-        while (read_io_port_u8(self.base_io_addr + 5) & 0x20) == 0 {
+        while (read_io_port_u8(self.base() + 5) & 0x20) == 0 {
             unsafe { asm!("pause") }
         }
-        write_io_port_u8(self.base_io_addr, c as u8)
+        write_io_port_u8(self.base(), c as u8)
     }
     pub fn try_read(&self) -> Option<u8> {
-        if read_io_port_u8(self.base_io_addr + 5) & 0x01 == 0 {
+        if read_io_port_u8(self.base() + 5) & 0x01 == 0 {
             None
         } else {
-            Some(read_io_port_u8(self.base_io_addr))
+            Some(read_io_port_u8(self.base()))
         }
     }
 
@@ -73,6 +63,6 @@ impl fmt::Write for SerialPort {
 }
 impl Default for SerialPort {
     fn default() -> Self {
-        Self::new(IO_ADDR_COM2)
+        Self::new(SerialPortIndex::Com2)
     }
 }
