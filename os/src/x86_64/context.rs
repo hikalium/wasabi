7603f6179537bc702585360d9943ee22194f2854
hikalium <hikalium@hikalium.com>
2024-02-09 16:26:25 +0900

    Move context switch code for syscall under context.rs

--- os/src/x86_64/context.rs
+++ os/src/x86_64/context.rs
@@ -2,6 +2,7 @@ use crate::error::Result;
 use crate::executor::yield_execution;
 use crate::mutex::Mutex;
 use core::arch::asm;
+use core::arch::global_asm;
 use core::mem::size_of;
 use core::mem::MaybeUninit;
 
@@ -224,3 +225,102 @@ pub async fn exec_app_context() -> Result<i64> {
     }
     Ok(retcode)
 }
+
+// https://doc.rust-lang.org/reference/items/external-blocks.html#abi
+// https://doc.rust-lang.org/reference/inline-assembly.html
+// https://www.intel.com/content/dam/develop/public/us/en/documents/325383-sdm-vol-2abcd.pdf#page=1401
+global_asm!(
+    // **** Symbols from Rust code
+    ".global arch_syscall_handler",
+    // **** Implementations
+    ".global asm_syscall_handler",
+    "asm_syscall_handler:",
+    // On syscall entry,
+    // RCX <= RIP to continue execution
+    // RIP <= IA32_LSTAR
+    // R11 <= RFLAGS
+    // RFLAGS &= IA32_FMASK
+    // CS <= {
+    //      Selector: IA32_STAR[32..48] & 0xFFFC
+    //      Base: 0
+    //      Attr: 64-bit code, accessed
+    //      DPL: 0
+    // }
+    // SS <= {
+    //      Selector: IA32_STAR[32..48] + 8
+    //      Base: 0
+    //      Attr: RW data, accessed
+    //      DPL: 0
+    // }
+
+    // Preserve registers after syscall
+    "sub rsp,64",
+    "push rsp",
+    "push r15",
+    "push r14",
+    "push r13",
+    "push r12",
+    "push 0", // r11 (destroyed)
+    "push r10",
+    "push r9",
+    "push r8",
+    "push rdi",
+    "push rsi",
+    "push rbp",
+    "push rbx",
+    "push rdx",
+    "push 0", // rcx (destroyed)
+    "push rax",
+    "push r11",     // RFLAGS saved on syscall
+    "push rcx",     // RIP saved on syscall
+    "sub rsp, 512", // Pseudo-FpuContext
+    //
+    "mov rbp, rsp", // Save rsp to restore later
+    "mov rdi, rsp", // First argument for syscall_handler (regs)
+    "and rsp, -16", // Align the stack (to satisfy sysv64 ABI)
+    "call arch_syscall_handler",
+    "mov rsp, rbp", // Recover original stack value
+    //
+    ".global return_to_app",
+    "return_to_app:",
+    // Restore registers to sysret
+    // This block assumes:
+    // - RSP = User stack, with saved registers
+    "add rsp, 512", // Pseudo-FpuContext
+    "pop rcx",      // RIP saved on syscall
+    "pop r11",      // RFLAGS saved on syscall
+    "pop rax",
+    "add rsp, 8", // rcx (destroyed)
+    "pop rdx",
+    "pop rbx",
+    "pop rbp",
+    "pop rsi",
+    "pop rdi",
+    "pop r8",
+    "pop r9",
+    "pop r10",
+    "add rsp, 8", // r11 (destroyed)
+    "pop r12",
+    "pop r13",
+    "pop r14",
+    "pop r15",
+    "add rsp,8", // rsp (skip)
+    "add rsp,64",
+    //
+    "sysretq",
+    // sysretq will do:
+    // RIP <= RCX
+    // RFLAGS <=  (R11 & 0x3C7FD7) | 2      // Clear RF, VM, reserved bits; set bit 1
+    // CS <= {
+    //      Selector: (IA32_STAR[48..64] + 16) | 3      // RPL = 3
+    //      Base: 0
+    //      Attr: 64-bit code, accessed
+    //      DPL: 3
+    // }
+    // SS <= {
+    //      Selector: (IA32_STAR[48..64] + 8) | 3       // RPL = 3
+    //      Base: 0
+    //      Attr: RW data, accessed
+    //      DPL: 3
+    // }
+);
