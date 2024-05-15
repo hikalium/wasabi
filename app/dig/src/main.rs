d8bce462106434b2fb01d098f0665500fdf086ac
hikalium <hikalium@hikalium.com>
2024-05-15 20:42:08 +0900

    Impl args deserialize and access from the apps

--- app/dig/src/main.rs
+++ app/dig/src/main.rs
@@ -2,31 +2,12 @@
 #![cfg_attr(not(target_os = "linux"), no_main)]
 
 extern crate alloc;
-use alloc::string::String;
+use noli::args;
 use noli::prelude::*;
 
 fn main() -> Result<()> {
-    let args = Api::get_args_region();
+    let args = args::from_env();
     println!("{args:?}");
-    let mut line = String::new();
-    println!("# Reverses the input string on a row.");
-    println!("# Type q and hit Enter to exit.");
-    loop {
-        if let Some(c) = Api::read_key() {
-            print!("{c}");
-            if c == '\n' {
-                if line == "q" {
-                    println!("bye!");
-                    break;
-                }
-                let reversed = line.chars().rev().collect::<String>();
-                println!("{reversed}");
-                line.clear();
-            } else {
-                line.push(c);
-            }
-        }
-    }
     Ok(())
 }
 
--- noli/src/args.rs
+++ noli/src/args.rs
@@ -1,5 +1,8 @@
 extern crate alloc;
 
+use crate::error::Error;
+use crate::error::Result;
+use crate::prelude::*;
 use alloc::vec::Vec;
 
 /// Serialize the arguments into bytes.
@@ -41,22 +44,58 @@ pub fn serialize_args(args: &[&str]) -> Vec<u8> {
     serialized
 }
 
+pub fn deserialize_args(data: &[u8]) -> Result<Vec<&str>> {
+    let mut tmp = [0u8; 8];
+
+    if data.len() < 16 {
+        return Err(Error::Failed("Invalid args data"));
+    }
+
+    tmp.copy_from_slice(&data[8..16]);
+    let num_args = u64::from_le_bytes(tmp) as usize;
+    if (num_args + 1) * 16 > data.len() {
+        return Err(Error::Failed("Invalid args data"));
+    }
+
+    let mut args = Vec::new();
+    for i in 0..num_args {
+        tmp.copy_from_slice(&data[(16 + i * 16)..(16 + i * 16 + 8)]);
+        let ofs = u64::from_le_bytes(tmp) as usize;
+        tmp.copy_from_slice(&data[(16 + i * 16 + 8)..(16 + i * 16 + 16)]);
+        let len = u64::from_le_bytes(tmp) as usize;
+        let s = core::str::from_utf8(&data[ofs..(ofs + len)]);
+        if let Ok(s) = s {
+            args.push(s);
+        } else {
+            return Err(Error::Failed("Invalid args data"));
+        }
+    }
+    Ok(args)
+}
+
+pub fn from_env() -> Vec<&'static str> {
+    let args = Api::get_args_region();
+    args.and_then(|args| deserialize_args(args).ok())
+        .unwrap_or_default()
+}
+
 #[cfg(test)]
 mod tests {
+    // For example, args ["zero", "one", "two"] will be
+    // serialized as follows:
+    // - total_size = 77
+    // - num_args = 3
+    // - ofs_and_len:
+    //   [0]: (ofs = 64, len = 4)
+    //   [1]: (ofs = 69, len = 3)
+    //   [2]: (ofs = 73, len = 3)
+    // - data = "zero\0one\0two\0"
+    use super::alloc::vec;
     use super::alloc::vec::Vec;
+    use super::deserialize_args;
     use super::serialize_args;
 
-    #[test]
-    fn example_serialize_args() {
-        // For example, args ["zero", "one", "two"] will be
-        // serialized as follows:
-        // - total_size = 77
-        // - num_args = 3
-        // - ofs_and_len:
-        //   [0]: (ofs = 64, len = 4)
-        //   [1]: (ofs = 69, len = 3)
-        //   [2]: (ofs = 73, len = 3)
-        // - data = "zero\0one\0two\0"
+    fn expected_bytes() -> Vec<u8> {
         let mut expected = Vec::new();
         expected.extend_from_slice(&77u64.to_le_bytes());
         expected.extend_from_slice(&3u64.to_le_bytes());
@@ -67,7 +106,22 @@ mod tests {
         expected.extend_from_slice(&73u64.to_le_bytes());
         expected.extend_from_slice(&3u64.to_le_bytes());
         expected.extend_from_slice("zero\0one\0two\0".as_bytes());
-        let actual = serialize_args(&["zero", "one", "two"]);
-        assert_eq!(actual, expected);
+        expected
+    }
+    fn expected_args() -> Vec<&'static str> {
+        vec!["zero", "one", "two"]
+    }
+
+    #[test]
+    fn test_serialize_args() {
+        let actual = serialize_args(&expected_args());
+        assert_eq!(actual, expected_bytes());
+    }
+
+    #[test]
+    fn test_deserialize_args() {
+        let input = expected_bytes();
+        let actual = deserialize_args(&input).unwrap();
+        assert_eq!(actual, expected_args());
     }
 }
