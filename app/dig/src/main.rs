4287e73ce87b9afaa796d15a7942b252e0d0ea42
hikalium <hikalium@hikalium.com>
2024-06-24 10:46:51 +0900

    update dig app

--- app/dig/src/main.rs
+++ app/dig/src/main.rs
@@ -3,11 +3,29 @@
 
 extern crate alloc;
 use noli::args;
+use noli::net::lookup_host;
 use noli::prelude::*;
 
 fn main() -> Result<()> {
     let args = args::from_env();
-    println!("{args:?}");
+    if args.len() <= 1 {
+        println!("Usage: dig <hostname> ...");
+        return Ok(());
+    }
+    for host in &args[1..] {
+        println!("{host}:");
+        match lookup_host(host) {
+            Ok(results) => {
+                println!("{} answers:", results.len());
+                for r in results {
+                    println!("  {r}")
+                }
+            }
+            e => {
+                println!("  {e:?}")
+            }
+        }
+    }
     Ok(())
 }
 
