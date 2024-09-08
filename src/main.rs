4df3c6ba50a149aace92b4955a553d5dad72f1bf
hikalium <hikalium@hikalium.com>
2024-09-09 00:19:27 +0900

    SKIP_TEST: infinite loop with no_std
    Change-Id: Ic845b22c67df259289f41e1585b3bf52b6b14772

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+fn efi_main() {
+    //println!("Hello, world!");
+    #[allow(clippy::empty_loop)]
+    loop {}
+}
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     #[allow(clippy::empty_loop)]
     loop {}
 }
