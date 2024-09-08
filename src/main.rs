f84e0f4d62c00d8de8fbce2b34f82415dbc903ae
hikalium <hikalium@hikalium.com>
2024-09-09 00:19:27 +0900

    SKIP_TEST: infinite loop with no_std
    Change-Id: Ic845b22c67df259289f41e1585b3bf52b6b14772

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+fn efi_main() {
+    //println!("Hello, world!");
+    loop {}
+}
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
