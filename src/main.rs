2cbd7e1b63905505824dfd91d2e8ee2dea969b11
hikalium <hikalium@hikalium.com>
2024-09-12 14:09:04 +0900

    SKIP_TEST: draw_font_fg
    Change-Id: Iaf2872ea516c74d0bc7b211601b19e18ada792c0

--- src/main.rs
+++ src/main.rs
@@ -116,32 +116,8 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
         let _ = draw_line(&mut vram, 0x00ffff, 0, 255, 255, i);
         let _ = draw_line(&mut vram, 0x00ffff, 255, 0, i, 255);
     }
-    let font_a = "
-........
-...**...
-...**...
-...**...
-...**...
-..*..*..
-..*..*..
-..*..*..
-..*..*..
-.******.
-.*....*.
-.*....*.
-.*....*.
-***..***
-........
-........
-";
-    for (y, row) in font_a.trim().split("\n").enumerate() {
-        for (x, pixel) in row.chars().enumerate() {
-            let color = match pixel {
-                '*' => 0xffffff,
-                _ => continue,
-            };
-            let _ = draw_point(&mut vram, color, x as i64, y as i64);
-        }
+    for (i, c) in "ABCDEF".chars().enumerate() {
+        draw_font_fg(&mut vram, i as i64 * 16 + 256, i as i64 * 16, 0xffffff, c)
     }
     //println!("Hello, world!");
     loop {
@@ -297,4 +273,36 @@ fn draw_line<T: Bitmap>(buf: &mut T, color: u32, x0: i64, y0: i64, x1: i64, y1: 
     draw_point(buf, color, x0, y0)?;
     draw_point(buf, color, x1, y1)?;
     Ok(())
+}
+
+fn draw_font_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, c: char) {
+    if let Ok(_c) = u8::try_from(c) {
+        let font_a = "
+........
+...**...
+...**...
+...**...
+...**...
+..*..*..
+..*..*..
+..*..*..
+..*..*..
+.******.
+.*....*.
+.*....*.
+.*....*.
+***..***
+........
+........
+";
+        for (dy, row) in font_a.trim().split("\n").enumerate() {
+            for (dx, pixel) in row.chars().enumerate() {
+                let color = match pixel {
+                    '*' => color,
+                    _ => continue,
+                };
+                let _ = draw_point(buf, color, x + dx as i64, y + dy as i64);
+            }
+        }
+    }
 }
