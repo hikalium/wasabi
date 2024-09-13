52e6888d0d8e0953f90c765fb166c9850d13399a
hikalium <hikalium@hikalium.com>
2024-09-13 16:36:46 +0900

    SKIP_TEST: draw_str_fg
    Change-Id: I7395f7ae5809b52322fccf2d74928f6fc656559c

--- src/main.rs
+++ src/main.rs
@@ -119,6 +119,7 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     for (i, c) in "ABCDEF".chars().enumerate() {
         draw_font_fg(&mut vram, i as i64 * 16 + 256, i as i64 * 16, 0xffffff, c)
     }
+    draw_str_fg(&mut vram, 256, 256, 0xffffff, "Hello, world!");
     //println!("Hello, world!");
     loop {
         hlt()
@@ -312,5 +313,11 @@ fn draw_font_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, c: char) {
                 let _ = draw_point(buf, color, x + dx as i64, y + dy as i64);
             }
         }
+    }
+}
+
+fn draw_str_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, s: &str) {
+    for (i, c) in s.chars().enumerate() {
+        draw_font_fg(buf, x + i as i64 * 8, y, color, c)
     }
 }
