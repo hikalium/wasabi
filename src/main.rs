0805ff5fe411fc70e2b3b3a24a9e3e06f7d291fe
hikalium <hikalium@hikalium.com>
2024-10-04 20:14:57 +0900

    Dump bytes of a root page table
    Change-Id: I2e3e9fbf518036a3978f47659138742de4085b03

--- src/main.rs
+++ src/main.rs
@@ -54,6 +54,9 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     )
     .unwrap();
     writeln!(w, "Hello, Non-UEFI world!").unwrap();
+    let cr3 = wasabi::x86::read_cr3();
+    println!("cr3 = {cr3:#p}");
+    hexdump(unsafe { &*cr3 });
     loop {
         hlt()
     }
