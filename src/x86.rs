77a1e19e90909e473ea6bd4d624875669bd22f7e
hikalium <hikalium@hikalium.com>
2024-10-04 21:52:54 +0900

    Define page table structure
    Change-Id: I00ac502259890376c3b306db8849f7765784f1cc

--- src/x86.rs
+++ src/x86.rs
@@ -1,4 +1,11 @@
+extern crate alloc;
+
+use crate::result::Result;
+use alloc::boxed::Box;
 use core::arch::asm;
+use core::fmt;
+use core::marker::PhantomData;
+use core::mem::MaybeUninit;
 
 pub fn hlt() {
     unsafe { asm!("hlt") }
@@ -25,10 +32,213 @@ pub fn write_io_port_u8(port: u16, data: u8) {
     }
 }
 
-pub type RootPageTable = [u8; 1024];
+pub const PAGE_SIZE: usize = 4096;
+const ATTR_MASK: u64 = 0xFFF;
+const ATTR_PRESENT: u64 = 1 << 0;
+const ATTR_WRITABLE: u64 = 1 << 1;
+const ATTR_USER: u64 = 1 << 2;
+const ATTR_WRITE_THROUGH: u64 = 1 << 3;
+const ATTR_CACHE_DISABLE: u64 = 1 << 4;
 
-pub fn read_cr3() -> *mut RootPageTable {
-    let mut cr3: *mut RootPageTable;
+#[derive(Debug, Copy, Clone)]
+#[repr(u64)]
+pub enum PageAttr {
+    NotPresent = 0,
+    ReadWriteKernel = ATTR_PRESENT | ATTR_WRITABLE,
+    ReadWriteUser = ATTR_PRESENT | ATTR_WRITABLE | ATTR_USER,
+    ReadWriteIo = ATTR_PRESENT | ATTR_WRITABLE | ATTR_WRITE_THROUGH | ATTR_CACHE_DISABLE,
+}
+#[derive(Debug, Eq, PartialEq)]
+pub enum TranslationResult {
+    PageMapped4K { phys: u64 },
+    PageMapped2M { phys: u64 },
+    PageMapped1G { phys: u64 },
+}
+
+#[repr(transparent)]
+pub struct Entry<const LEVEL: usize, const SHIFT: usize, NEXT> {
+    value: u64,
+    next_type: PhantomData<NEXT>,
+}
+impl<const LEVEL: usize, const SHIFT: usize, NEXT> Entry<LEVEL, SHIFT, NEXT> {
+    fn read_value(&self) -> u64 {
+        self.value
+    }
+    fn is_present(&self) -> bool {
+        (self.read_value() & (1 << 0)) != 0
+    }
+    fn is_writable(&self) -> bool {
+        (self.read_value() & (1 << 1)) != 0
+    }
+    fn is_user(&self) -> bool {
+        (self.read_value() & (1 << 2)) != 0
+    }
+    fn format(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "L{}Entry @ {:#p} {{ {:#018X} {}{}{} ",
+            LEVEL,
+            self,
+            self.read_value(),
+            if self.is_present() { "P" } else { "N" },
+            if self.is_writable() { "W" } else { "R" },
+            if self.is_user() { "U" } else { "S" }
+        )?;
+        write!(f, " }}")
+    }
+    fn table(&self) -> Result<&NEXT> {
+        if self.is_present() {
+            Ok(unsafe { &*((self.value & !ATTR_MASK) as *const NEXT) })
+        } else {
+            Err("Page Not Found")
+        }
+    }
+    fn table_mut(&mut self) -> Result<&mut NEXT> {
+        if self.is_present() {
+            Ok(unsafe { &mut *((self.value & !ATTR_MASK) as *mut NEXT) })
+        } else {
+            Err("Page Not Found")
+        }
+    }
+    fn page(&self) -> Result<u64> {
+        if self.is_present() {
+            Ok(self.value & !ATTR_MASK)
+        } else {
+            Err("Page Not Found")
+        }
+    }
+    fn populate(&mut self) -> Result<&mut Self> {
+        if self.is_present() {
+            Err("Page is already populated")
+        } else {
+            let next: Box<NEXT> = Box::new(unsafe { MaybeUninit::zeroed().assume_init() });
+            self.value = Box::into_raw(next) as u64 | PageAttr::ReadWriteUser as u64;
+            Ok(self)
+        }
+    }
+    fn ensure_populated(&mut self) -> Result<&mut Self> {
+        if self.is_present() {
+            Ok(self)
+        } else {
+            self.populate()
+        }
+    }
+    fn set_page(&mut self, phys: u64, attr: PageAttr) -> Result<()> {
+        if phys & ATTR_MASK != 0 {
+            Err("phys is not aligned")
+        } else {
+            self.value = phys | attr as u64;
+            Ok(())
+        }
+    }
+}
+impl<const LEVEL: usize, const SHIFT: usize, NEXT> fmt::Display for Entry<LEVEL, SHIFT, NEXT> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.format(f)
+    }
+}
+impl<const LEVEL: usize, const SHIFT: usize, NEXT> fmt::Debug for Entry<LEVEL, SHIFT, NEXT> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.format(f)
+    }
+}
+
+#[repr(align(4096))]
+pub struct Table<const LEVEL: usize, const SHIFT: usize, NEXT> {
+    entry: [Entry<LEVEL, SHIFT, NEXT>; 512],
+}
+impl<const LEVEL: usize, const SHIFT: usize, NEXT: core::fmt::Debug> Table<LEVEL, SHIFT, NEXT> {
+    fn format(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "L{}Table @ {:#p} {{", LEVEL, self)?;
+        for i in 0..512 {
+            let e = &self.entry[i];
+            if !e.is_present() {
+                continue;
+            }
+            writeln!(f, "  entry[{:3}] = {:?}", i, e)?;
+        }
+        writeln!(f, "}}")
+    }
+    fn calc_index(&self, addr: u64) -> usize {
+        ((addr >> SHIFT) & 0b1_1111_1111) as usize
+    }
+    pub fn next_level(&self, index: usize) -> Option<&NEXT> {
+        self.entry.get(index).and_then(|e| e.table().ok())
+    }
+}
+impl<const LEVEL: usize, const SHIFT: usize, NEXT: fmt::Debug> fmt::Debug
+    for Table<LEVEL, SHIFT, NEXT>
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.format(f)
+    }
+}
+
+pub type PT = Table<1, 12, [u8; PAGE_SIZE]>;
+pub type PD = Table<2, 21, PT>;
+pub type PDPT = Table<3, 30, PD>;
+pub type PML4 = Table<4, 39, PDPT>;
+
+impl PML4 {
+    pub fn new() -> Box<Self> {
+        Box::new(Self::default())
+    }
+    fn default() -> Self {
+        // This is safe since entries filled with 0 is valid.
+        unsafe { MaybeUninit::zeroed().assume_init() }
+    }
+    pub fn create_mapping(
+        &mut self,
+        virt_start: u64,
+        virt_end: u64,
+        phys: u64,
+        attr: PageAttr,
+    ) -> Result<()> {
+        if virt_start & ATTR_MASK != 0 {
+            return Err("Invalid virt_start");
+        }
+        if virt_end & ATTR_MASK != 0 {
+            return Err("Invalid virt_end");
+        }
+        if phys & ATTR_MASK != 0 {
+            return Err("Invalid phys");
+        }
+        for addr in (virt_start..virt_end).step_by(PAGE_SIZE) {
+            let index = self.calc_index(addr);
+            let table = self.entry[index].ensure_populated()?.table_mut()?;
+            let index = table.calc_index(addr);
+            let table = table.entry[index].ensure_populated()?.table_mut()?;
+            let index = table.calc_index(addr);
+            let table = table.entry[index].ensure_populated()?.table_mut()?;
+            let index = table.calc_index(addr);
+            let pte = &mut table.entry[index];
+            pte.set_page(phys + addr - virt_start, attr)?;
+        }
+        Ok(())
+    }
+    pub fn translate(&self, virt: u64) -> Result<TranslationResult> {
+        let index = self.calc_index(virt);
+        let entry = &self.entry[index];
+        let table = entry.table()?;
+
+        let index = table.calc_index(virt);
+        let entry = &table.entry[index];
+        let table = entry.table()?;
+
+        let index = table.calc_index(virt);
+        let entry = &table.entry[index];
+        let table = entry.table()?;
+
+        let index = table.calc_index(virt);
+        let entry = &table.entry[index];
+        let page = entry.page()?;
+
+        Ok(TranslationResult::PageMapped4K { phys: page })
+    }
+}
+
+pub fn read_cr3() -> *mut PML4 {
+    let mut cr3: *mut PML4;
     unsafe {
         asm!("mov rax, cr3",
             out("rax") cr3)
