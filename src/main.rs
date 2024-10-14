0a98f1b0ef6ee5047f061089970b7718d0a4d0c0
hikalium <hikalium@hikalium.com>
2024-10-14 16:51:39 +0900

    Remove null pointer dereferences
    Change-Id: Ia10e7bf216af2e2d2716b71df46f1fcacf22e969

--- src/main.rs
+++ src/main.rs
@@ -83,13 +83,7 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     init_paging(&memory_map);
     info!("Now we are using our own page tables!");
 
-    info!("Reading from memory address 0...");
-
-    #[allow(clippy::zero_ptr)]
-    #[allow(deref_nullptr)]
-    let value_at_zero = unsafe { *(0 as *const u8) };
-    info!("value_at_zero = {value_at_zero}");
-
+    // Unmap page 0 to detect null ptr dereference
     let page_table = read_cr3();
     unsafe {
         (*page_table)
@@ -97,12 +91,6 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
             .expect("Failed to unmap page 0");
     }
     flush_tlb();
-
-    info!("Reading from memory address 0... (again)");
-    #[allow(clippy::zero_ptr)]
-    #[allow(deref_nullptr)]
-    let value_at_zero = unsafe { *(0 as *const u8) };
-    info!("value_at_zero = {value_at_zero}");
 
     loop {
         hlt()
