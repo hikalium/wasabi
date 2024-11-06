ae307d830a23dddd9641af745c77ad9cd6b331e5
hikalium <hikalium@hikalium.com>
2024-11-06 17:07:30 +0900

    xhci: alloc required structures for init
    Change-Id: I82c23dbb3247b8921818c6aa76fd97481da2e51e

--- src/xhci.rs
+++ src/xhci.rs
@@ -1,13 +1,25 @@
+extern crate alloc;
+
+use crate::allocator::ALLOCATOR;
 use crate::bits::extract_bits;
+use crate::executor::spawn_global;
 use crate::info;
 use crate::mmio::Mmio;
+use crate::mutex::Mutex;
 use crate::pci::BarMem64;
 use crate::pci::BusDeviceFunction;
 use crate::pci::Pci;
 use crate::pci::VendorDeviceId;
 use crate::result::Result;
 use crate::volatile::Volatile;
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+use core::alloc::Layout;
+use core::cmp::max;
 use core::marker::PhantomPinned;
+use core::mem::MaybeUninit;
+use core::pin::Pin;
+use core::slice;
 
 pub struct PciXhciDriver {}
 impl PciXhciDriver {
@@ -52,15 +64,21 @@ impl PciXhciDriver {
         pci.enable_bus_master(bdf)?;
         let bar0 = pci.try_bar0_mem64(bdf)?;
         bar0.disable_cache();
-        info!("xhci: {bar0:?}");
         let (cap_regs, op_regs, rt_regs) = Self::setup_xhc_registers(&bar0)?;
+        let scratchpad_buffers = ScratchpadBuffers::alloc(cap_regs.as_ref(), op_regs.as_ref())?;
+        let device_context_base_array = DeviceContextBaseAddressArray::new(scratchpad_buffers);
+        let xhc = Controller::new(cap_regs, op_regs, rt_regs, device_context_base_array);
+        spawn_global(Self::run(xhc));
+        Ok(())
+    }
+    async fn run(xhc: Controller) -> Result<()> {
         info!(
             "xhci: cap_regs.MaxSlots = {}",
-            cap_regs.as_ref().num_of_device_slots()
+            xhc.cap_regs.as_ref().num_of_device_slots()
         );
-        info!("xhci: op_regs.USBSTS = {}", op_regs.as_ref().usbsts());
-        info!("xhci: rt_regs.MFINDEX = {}", rt_regs.as_ref().mfindex());
-        Err("wip")
+        info!("xhci: op_regs.USBSTS = {}", xhc.op_regs.as_ref().usbsts());
+        info!("xhci: rt_regs.MFINDEX = {}", xhc.rt_regs.as_ref().mfindex());
+        Ok(())
     }
 }
 
@@ -88,6 +106,10 @@ impl CapabilityRegisters {
     pub fn num_of_device_slots(&self) -> usize {
         extract_bits(self.hcsparams1.read(), 0, 8) as usize
     }
+    pub fn num_scratchpad_bufs(&self) -> usize {
+        (extract_bits(self.hcsparams2.read(), 21, 5) << 5
+            | extract_bits(self.hcsparams2.read(), 27, 5)) as usize
+    }
 }
 
 // [xhci_1_2] p.31
@@ -98,10 +120,17 @@ impl CapabilityRegisters {
 // Address Array is utilized by the xHCI Scratchpad mechanism.
 #[repr(C, align(64))]
 pub struct RawDeviceContextBaseAddressArray {
-    context: [u64; 256],
+    scratchpad_table_ptr: *const *const u8,
+    context: [u64; 255],
     _pinned: PhantomPinned,
 }
 const _: () = assert!(size_of::<RawDeviceContextBaseAddressArray>() == 2048);
+impl RawDeviceContextBaseAddressArray {
+    fn new() -> Self {
+        unsafe { MaybeUninit::zeroed().assume_init() }
+    }
+}
+
 #[repr(C)]
 pub struct OperationalRegisters {
     usbcmd: Volatile<u32>,
@@ -118,6 +147,15 @@ const _: () = assert!(size_of::<OperationalRegisters>() == 0x40);
 impl OperationalRegisters {
     fn usbsts(&self) -> u32 {
         self.usbsts.read()
+    }
+    pub fn page_size(&self) -> Result<usize> {
+        let page_size_bits = self.pagesize.read() & 0xFFFF;
+        // bit[n] of page_size_bits is set => PAGE_SIZE will be 2^(n+12).
+        if page_size_bits.count_ones() != 1 {
+            return Err("PAGE_SIZE has multiple bits set");
+        }
+        let page_size_shift = page_size_bits.trailing_zeros();
+        Ok(1 << (page_size_shift + 12))
     }
 }
 
@@ -142,5 +180,106 @@ const _: () = assert!(size_of::<RuntimeRegisters>() == 0x8020);
 impl RuntimeRegisters {
     fn mfindex(&self) -> u32 {
         self.mfindex.read()
+    }
+}
+
+struct ScratchpadBuffers {
+    table: Pin<Box<[*const u8]>>,
+    _bufs: Vec<Pin<Box<[u8]>>>,
+}
+impl ScratchpadBuffers {
+    fn alloc(cap_regs: &CapabilityRegisters, op_regs: &OperationalRegisters) -> Result<Self> {
+        let page_size = op_regs.page_size()?;
+        info!("xhci: page_size = {page_size}");
+        let num_scratchpad_bufs = cap_regs.num_scratchpad_bufs();
+        info!("xhci: original num_scratchpad_bufs = {num_scratchpad_bufs}");
+
+        let num_scratchpad_bufs = max(cap_regs.num_scratchpad_bufs(), 1);
+        let table = ALLOCATOR.alloc_with_options(
+            Layout::from_size_align(size_of::<usize>() * num_scratchpad_bufs, page_size)
+                .map_err(|_| "could not allocate scratchpad buffer table")?,
+        );
+        let table = unsafe { slice::from_raw_parts(table as *mut *const u8, num_scratchpad_bufs) };
+        let mut table = Pin::new(Box::<[*const u8]>::from(table));
+        let mut bufs = Vec::new();
+        for sb in table.iter_mut() {
+            let buf = ALLOCATOR.alloc_with_options(
+                Layout::from_size_align(page_size, page_size)
+                    .map_err(|_| "could not allocated a scratchpad buffer")?,
+            );
+            let buf = unsafe { slice::from_raw_parts(buf as *const u8, page_size) };
+            let buf = Pin::new(Box::<[u8]>::from(buf));
+            *sb = buf.as_ref().as_ptr();
+            bufs.push(buf);
+        }
+        Ok(Self { table, _bufs: bufs })
+    }
+}
+
+#[repr(C, align(32))]
+#[derive(Default, Debug)]
+pub struct EndpointContext {
+    data: [u32; 2],
+    tr_dequeue_ptr: Volatile<u64>,
+    average_trb_length: u16,
+    max_esit_payload_low: u16,
+    _reserved: [u32; 3],
+}
+const _: () = assert!(size_of::<EndpointContext>() == 0x20);
+
+#[repr(C, align(32))]
+#[derive(Default)]
+pub struct DeviceContext {
+    slot_ctx: [u32; 8],
+    ep_ctx: [EndpointContext; 2 * 15 + 1],
+}
+const _: () = assert!(size_of::<DeviceContext>() == 0x400);
+
+const _: () = assert!(size_of::<DeviceContext>() == 0x400);
+#[repr(C, align(4096))]
+#[derive(Default)]
+pub struct OutputContext {
+    device_ctx: DeviceContext,
+    _pinned: PhantomPinned,
+}
+const _: () = assert!(size_of::<OutputContext>() <= 4096);
+
+struct DeviceContextBaseAddressArray {
+    _inner: Pin<Box<RawDeviceContextBaseAddressArray>>,
+    _context: [Option<Pin<Box<OutputContext>>>; 255],
+    _scratchpad_buffers: ScratchpadBuffers,
+}
+impl DeviceContextBaseAddressArray {
+    pub fn new(scratchpad_buffers: ScratchpadBuffers) -> Self {
+        let mut inner = RawDeviceContextBaseAddressArray::new();
+        inner.scratchpad_table_ptr = scratchpad_buffers.table.as_ref().as_ptr();
+        Self {
+            _inner: Box::pin(inner),
+            _context: unsafe { MaybeUninit::zeroed().assume_init() },
+            _scratchpad_buffers: scratchpad_buffers,
+        }
+    }
+}
+
+struct Controller {
+    cap_regs: Mmio<CapabilityRegisters>,
+    op_regs: Mmio<OperationalRegisters>,
+    rt_regs: Mmio<RuntimeRegisters>,
+    _device_context_base_array: Mutex<DeviceContextBaseAddressArray>,
+}
+impl Controller {
+    pub fn new(
+        cap_regs: Mmio<CapabilityRegisters>,
+        op_regs: Mmio<OperationalRegisters>,
+        rt_regs: Mmio<RuntimeRegisters>,
+        device_context_base_array: DeviceContextBaseAddressArray,
+    ) -> Self {
+        let device_context_base_array = Mutex::new(device_context_base_array);
+        Self {
+            cap_regs,
+            op_regs,
+            rt_regs,
+            _device_context_base_array: device_context_base_array,
+        }
     }
 }
