07de6db28872a99e53bec52a75a25c76e8699678
hikalium <hikalium@hikalium.com>
2024-11-12 18:17:35 +0900

    Connect USB Keyboard device to QEMU
    Change-Id: I9ffd3720825b7eee6ab344fa4f30e0c715108821

--- scripts/launch_qemu.sh
+++ scripts/launch_qemu.sh
@@ -16,6 +16,7 @@ qemu-system-x86_64 \
   -serial chardev:char_com1 \
   -monitor telnet:0.0.0.0:2345,server,nowait,logfile=log/qemu_monitor.txt \
   -device qemu-xhci \
+  -device usb-kbd \
   -device isa-debug-exit,iobase=0xf4,iosize=0x01
 RETCODE=$?
 set -e
