382ade2a156a1827518224615d1aec03f0760f3d
hikalium <hikalium@hikalium.com>
2024-12-03 00:12:42 +0900

    Impl extract_bits_from_le_bytes
    Change-Id: I9d7e81a6d93a4ab1f8aeb2bf21f9f071ea17fef0

--- src/bits.rs
+++ src/bits.rs
@@ -30,3 +30,42 @@ fn extract_bits_tests() {
         0x1234_5678_1234_5678u64
     );
 }
+
+pub fn extract_bits_from_le_bytes(bytes: &[u8], shift: usize, width: usize) -> Option<u64> {
+    if width == 0 {
+        return None;
+    }
+    let byte_range = (shift / 8)..((shift + width + 7) / 8);
+    let mut value = 0u64;
+    let bit_shift = shift - byte_range.start * 8;
+    bytes.get(byte_range).map(|bytes_in_range| {
+        for (i, v) in bytes_in_range.iter().enumerate() {
+            let v = *v as u128;
+            value |= ((v << (i * 8)) >> bit_shift) as u64;
+        }
+        extract_bits(value, 0, width)
+    })
+}
+
+#[test_case]
+fn extract_bits_from_le_bytes_tests() {
+    assert_eq!(extract_bits_from_le_bytes(&[], 0, 0), None);
+    assert_eq!(extract_bits_from_le_bytes(&[], 0, 1), None);
+    assert_eq!(extract_bits_from_le_bytes(&[], 1, 0), None);
+    assert_eq!(
+        extract_bits_from_le_bytes(&[0b01010101, 0b10101010], 0, 0),
+        None
+    );
+    assert_eq!(
+        extract_bits_from_le_bytes(&[0b01010101, 0b10101010], 0, 8),
+        Some(0b01010101)
+    );
+    assert_eq!(
+        extract_bits_from_le_bytes(&[0b01010101, 0b10101010], 8, 8),
+        Some(0b10101010)
+    );
+    assert_eq!(
+        extract_bits_from_le_bytes(&[0b01010101, 0b10101010], 4, 8),
+        Some(0b10100101)
+    );
+}
