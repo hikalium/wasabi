66749d96f61f89b21901bfbde25097ddc16af595
hikalium <hikalium@hikalium.com>
2026-07-13 07:17:02 +0900

    book3/ch1: tablet: paint mouse cursor moves
    Change-Id: Ibb31f3a89376f28f09abec6bcd949e6b4ce74af4

--- src/graphics.rs
+++ src/graphics.rs
@@ -48,8 +48,7 @@ unsafe fn unchecked_draw_point<T: Bitmap>(
 ) {
     *buf.unchecked_pixel_at_mut(x, y) = color;
 }
-
-fn draw_point<T: Bitmap>(
+pub fn draw_point<T: Bitmap>(
     buf: &mut T,
     color: u32,
     x: i64,
